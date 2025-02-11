81c7cf2851af76d495e259436015fb3583452009
TheJamieP <jamieelliotpunshon@gmail.com>
2025-02-11 21:09:57 +0000

    Refactor: allow multi word cars

--- commands/fetchcar.ts
+++ commands/fetchcar.ts
@@ -5,10 +5,13 @@ type Car = {
 export default async (message: any, args: string[]): Promise<void> => {
     if (!args) return message.reply("Please include a valid vehicle name, e.g: $fetchcar adder");
 
-    let Car: string = args[0];
+    console.log(args)
     
+    let car: string = "";
+    if (args.length > 1) args.forEach((word: string) => {car += args.indexOf(word) != args.length ? `${word} ` : word})
+    else car = args[0]
 
-    fetch(`https://gta.vercel.app/api/vehicles/${Car.toLowerCase()}`, {
+    fetch(`https://gta.vercel.app/api/vehicles/${car.toLowerCase()}`, {
         "method": "GET"
     }).then((res) => res.json() as Promise<Car>).then( async (res: Car) => {
         if (!res) return;
